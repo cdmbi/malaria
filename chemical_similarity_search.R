3766150b45c04a3cb635ad534aef53a3067c6285
Saw Simeon <doe_doe207@hotmail.com>
2016-03-23 21:22:02 +0700

    adjust script

--- chemical_similarity_search.R
+++ chemical_similarity_search.R
@@ -7,24 +7,29 @@ library(data.table)
 target_data <- fread("ChEMBL_21_MWTlt900_standardized.csv")
 query_data <- read.csv("GAMO_PFdata_200115.csv", stringsAsFactors = FALSE)
 query_smiles <- query_data$smiles[1:13403]
-target_smiles <- target_data$smiles[1:100000]
+target_smiles <- target_data$smiles[110001:120000]
+
+sample <- fread("results_10000.csv")
 
 ##calculating all 
 options(java.parameters = "-Xmx31000m")
 target.mols <- parse.smiles(target_smiles)
-cl <- makeCluster(24)
-clusterExport(cl = cl, varlist = "target.mols")
-target.fps <- parLapply(cl = cl,
-                        target.mols,
-                        get.fingerprint, type = "circular")
+#library(parallel)
+#cl <- makeCluster(24)
+#clusterExport(cl = cl, varlist = "target.mols")
+#target.fps <- parLapply(cl = cl,
+#                        target.mols,
+#                        get.fingerprint, type = "circular")
+target.fps <- lapply(target.mols, get.fingerprint, type = "circular")
+
+saveRDS(target.fps, "target_fps_120000.Rds")
 
 
 
+query_mols <- parse.smiles(query_smiles)[[1]]
+target.mols <- parse.smiles(target_smiles)
 
-#query_mols <- parse.smiles(query_smiles)[[1]]
-#target.mols <- parse.smiles(target_smiles)
-
-#query.fp <- get.fingerprint(query_mols, type = "circular")
+query.fp <- get.fingerprint(query_mols, type = "circular")
 #target.fps <- lapply(target.mols, get.fingerprint, type = "circular")
 #tanimoto_similarity <- unlist(lapply(target.fps, distance, fp2 = query.fp,
 #                                     method = 'tanimoto'))
@@ -32,16 +37,24 @@ target.fps <- parLapply(cl = cl,
 
 
 #my_results <- data.frame()
+setwd("~/Documents/malaria")
+target_data <- readRDS("target_fps_30000.Rds")
+query_data <- readRDS("query_fp_GAMPO.Rds")
+
 library(parallel)
 library(doSNOW)
 cl <- makeCluster(24)
+#clusterExport(cl, "target.fps")
 registerDoSNOW(cl)
-my_results <- list(13403)
+my_results <- list(1:13403)
 my_results <- foreach(i = 1:13403, .packages = 'rcdk') %dopar% {
-  query.mol <- parse.smiles(query_smiles)[[i]]
-  target.mols <- parse.smiles(target_smiles)
-  query.fp <- get.fingerprint(query.mol, type = "circular")
-  target.fps <- lapply(target.mols, get.fingerprint, type = "circular")
+  #query.mol <- parse.smiles(query_smiles)[[i]]
+  #target.mols <- parse.smiles(target_smiles)
+  #query.fp <- get.fingerprint(query.mol, type = "circular")
+  query.fp <- query_data[[i]]
+  #target.fps <- lapply(target.mols, get.fingerprint, type = "circular")
+  #target.fps <- readRDS("target_fps_10000.Rds")
+  target.fps <- target_data
   tanimoto_similarity <- unlist(lapply(target.fps, distance,
                                        fp2 = query.fp,
                                        method = "tanimoto"))
@@ -50,4 +63,8 @@ my_results <- foreach(i = 1:13403, .packages = 'rcdk') %dopar% {
   rm(query.fp)
   rm(target.fps)
   my_results[[i]] <- tanimoto_similarity
-}
+                      }
+
+my_results_df <- as.data.frame(do.call("rbind", my_results))
+
+write.csv(my_results_df, file = "results_300000.csv")
